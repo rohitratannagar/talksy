8b87a9921045e2735dfe932ea5263e18dc6f90a4
Rohit Ratan Nagar <rohitratannagar2003@gmail.com>
2025-04-23 20:18:04 +0530

    render

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,23 +18,13 @@ const __dirname = path.resolve();
 
 app.use(express.json({limit: '10mb'}));
 app.use(cookieParser());
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-// }));
-
-const allowedOrigins = ['http://localhost:5173', 'http://192.168.0.153:5173'];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+    origin: "http://localhost:5173",
+    credentials: true,
 }));
+
+
+
 
 
 
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,9 +3,8 @@ import {protectRoute} from "../middlewares/auth.middleware.js"
 import {getUsersForSideBar, getMessages, sendMessage, deleteMessage} from "../controllers/message.controller.js"
 const router = express.Router();
 
-router.get('/users', protectRoute, getUsersForSideBar);
-
 router.post('/send/:id', protectRoute, sendMessage);
 router.delete('/delete/:id', protectRoute, deleteMessage);
+router.get('/users', protectRoute, getUsersForSideBar);
 router.get('/:id', protectRoute, getMessages)
 export default router;
