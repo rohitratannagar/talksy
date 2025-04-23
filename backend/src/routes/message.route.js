008c0e4bba21880cb6e585222f95a06169ec1ca3
Rohit Ratan Nagar <rohitratannagar2003@gmail.com>
2025-04-23 19:51:51 +0530

    change in route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -4,7 +4,8 @@ import {getUsersForSideBar, getMessages, sendMessage, deleteMessage} from "../co
 const router = express.Router();
 
 router.get('/users', protectRoute, getUsersForSideBar);
-router.get('/:id', protectRoute, getMessages)
+
 router.post('/send/:id', protectRoute, sendMessage);
 router.delete('/delete/:id', protectRoute, deleteMessage);
+router.get('/:id', protectRoute, getMessages)
 export default router;
